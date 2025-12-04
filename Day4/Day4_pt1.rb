75d9c3c27ae9b3fa262776a4bfe33d7d2d21a844
Lara Luís <lr_lara_raquel@hotmail.com>
2025-12-04 07:56:36 +0000

    Day 4

---
+++ Day4/Day4_pt1.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+class Day4
+  def initialize
+    @map = []
+  end
+
+  def read_map
+    File.readlines('Day4\\input4.txt').each do |line|
+      @map << line.chomp.chars
+    end
+  end
+
+  def count_roles
+    roles_count = 0
+    
+    @map.each_with_index do |row, i|
+      row.each_with_index do |cell, j|
+        next unless cell == '@'
+
+        surrounding_roles = 0
+
+        surrounding_roles += 1 if i.positive? && j.positive? && @map[i - 1][j - 1] == '@' # Top-left
+        surrounding_roles += 1 if i.positive? && @map[i - 1][j] == '@'               # Top
+        surrounding_roles += 1 if i.positive? && j < row.length - 1 && @map[i - 1][j + 1] == '@' # Top-right
+        surrounding_roles += 1 if j.positive? && @map[i][j - 1] == '@'               # Left
+        surrounding_roles += 1 if j < row.length - 1 && @map[i][j + 1] == '@' # Right
+        surrounding_roles += 1 if i < @map.length - 1 && j.positive? && @map[i + 1][j - 1] == '@' # Bottom-left
+        surrounding_roles += 1 if i < @map.length - 1 && @map[i + 1][j] == '@' # Bottom
+        surrounding_roles += 1 if i < @map.length - 1 && j < row.length - 1 && @map[i + 1][j + 1] == '@' # Bottom-right
+
+        roles_count += 1 if surrounding_roles < 4
+      end
+    end
+
+    roles_count
+  end
+end
+
+day4 = Day4.new
+day4.read_map
+puts day4.count_roles
